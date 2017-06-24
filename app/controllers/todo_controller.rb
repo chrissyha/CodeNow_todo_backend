b17bbbcd955a6f13bd4d2d009bdd7a3e5bc88e50
Chrissy Ha <christinayha@gmail.com>
2017-06-24 20:15:20 +0000

    Formatting fixes

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -6,7 +6,7 @@ class TodoController < ApplicationController
     @todo_description = "New to do"
     todo_id = params[:id]
 
-    if todo_id == '1' # Remember, the `==` conditional operator is used to check if two things are equal to each other.
+    if todo_id == '1'
       @todo_description = "Pick trails"
     elsif todo_id == '2'
       @todo_description = "Pack basics: tent, pack, sleeping bag, sleeping pad, bladder, poles"
