692e90ae8fc8da457de8c248a5864cd0e0a60819
Chrissy Ha <christinayha@gmail.com>
2017-06-24 16:16:42 +0000

    Add conditional logic to show action of To do controller

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -4,5 +4,12 @@ class TodoController < ApplicationController
   
   def show
     @todo_description = "New to do"
+    todo_id = params[:id]
+
+    if todo_id == '1' # Remember, the `==` conditional operator is used to check if two things are equal to each other.
+      @todo_description = "Pick trails"
+    elsif todo_id == '2'
+      @todo_description = "Pack basics: tent, pack, sleeping bag, sleeping pad, bladder, poles"
+    end
   end
 end
